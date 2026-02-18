4a0522dcc24e4d67c8f6a898ba6991a035be8b25
𝔸𝕣𝕒𝕤𝕙 <44658653+6arash6@users.noreply.github.com>
2026-02-18 11:19:26 +0000

    Add ft_create_map.c with functions for map creation and memory management
    Co-authored-by: mina-zahir <mina-zahir@users.noreply.github.com>

---
+++ bsq/src/ft_create_map.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_map.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: student <student@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/01 00:00:00 by student          #+#    #+#             */
+/*   Updated: 2024/01/01 00:00:00 by student         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bsq.h"
+
+t_map    *ft_create_map(char *filename)
+{
+    t_map    *map;
+
+    map = (t_map *)malloc(sizeof(t_map));
+    if (!map)
+        return (NULL);
+    map->matrix = NULL;
+    map->rows = 0;
+    map->cols = 0;
+    if (!ft_get_matrix(map, filename))
+    {
+        free(map);
+        return (NULL);
+    }
+    return (map);
+}
+
+t_map    *ft_create_map_from_stdin(void)
+{
+    t_map    *map;
+
+    map = (t_map *)malloc(sizeof(t_map));
+    if (!map)
+        return (NULL);
+    map->matrix = NULL;
+    map->rows = 0;
+    map->cols = 0;
+    if (!ft_get_matrix_from_stdin(map))
+    {
+        free(map);
+        return (NULL);
+    }
+    return (map);
+}
+
+void    ft_free_map(t_map *map)
+{
+    int    i;
+
+    if (!map)
+        return ;
+    if (map->matrix)
+    {
+        i = 0;
+        while (i < map->rows)
+        {
+            if (map->matrix[i])
+                free(map->matrix[i]);
+            i++;
+        }
+        free(map->matrix);
+    }
+    free(map);
+}
