9d61b7405053a3df7433dc66f671ec9baf70b9bd
𝔸𝕣𝕒𝕤𝕙 <44658653+6arash6@users.noreply.github.com>
2026-02-18 14:18:10 +0000

    Add ft_memory.c: implement memory management functions including ft_realloc, ft_isnumber, and ft_atoi
    Co-authored-by: Mina Zahir <mina-zahir@users.noreply.github.com>

---
+++ bsq/src/ft_memory.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memory.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzahirjo <mzahirjo@student.42vienna.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/18 00:00:00 by mzahirjo          #+#    #+#             */
+/*   Updated: 2026/02/18 00:00:00 by mzahirjo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bsq.h"
+
+char	*ft_realloc(char *ptr, int old_size, int new_size)
+{
+	char	*new_ptr;
+	int		i;
+
+	new_ptr = (char *)malloc(new_size);
+	if (!new_ptr)
+		return (NULL);
+	i = 0;
+	while (i < old_size)
+	{
+		new_ptr[i] = ptr[i];
+		i++;
+	}
+	if (ptr)
+		free(ptr);
+	return (new_ptr);
+}
+
+int	ft_isnumber(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	i;
+
+	result = 0;
+	i = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result);
+}
