72274c16e91ca478d518f3c75f3f70bc04794378
𝔸𝕣𝕒𝕤𝕙 <44658653+6arash6@users.noreply.github.com>
2026-02-18 14:18:36 +0000

    Refactor main.c: extract file and stdin processing into separate functions for improved readability
    Co-authored-by: Mina Zahir <mina-zahir@users.noreply.github.com>

--- bsq/src/main.c
+++ bsq/src/main.c
@@ -1,34 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzahirjo <mzahirjo@student.42vienna.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/18 00:00:00 by mzahirjo          #+#    #+#             */
+/*   Updated: 2026/02/18 00:00:00 by mzahirjo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "bsq.h"
 
-int    main(int argc, char **argv)
+static void	ft_process_file(char *filename)
 {
-    t_map    *map;
-    int        i;
+	t_map	map;
 
-    if (argc == 1)
-    {
-        map = ft_create_map_from_stdin();
-        if (map)
-        {
-            ft_process_map(map);
-            ft_free_map(map);
-        }
-    }
-    else
-    {
-        i = 1;
-        while (i < argc)
-        {
-            map = ft_create_map(argv[i]);
-            if (map)
-            {
-                ft_process_map(map);
-                ft_free_map(map);
-            }
-            if (i < argc - 1)
-                ft_putchar('\n');
-            i++;
-        }
-    }
-    return (0);
+	map.matrix = NULL;
+	if (!ft_get_matrix(&map, filename))
+		return ;
+	ft_process_map(&map);
+	ft_print_map(&map);
+	ft_free_map(&map);
+}
+
+static void	ft_process_stdin(void)
+{
+	t_map	map;
+
+	map.matrix = NULL;
+	if (!ft_get_matrix_from_stdin(&map))
+		return ;
+	ft_process_map(&map);
+	ft_print_map(&map);
+	ft_free_map(&map);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc == 1)
+		ft_process_stdin();
+	else
+	{
+		i = 1;
+		while (i < argc)
+		{
+			ft_process_file(argv[i]);
+			i++;
+		}
+	}
+	return (0);
 }
